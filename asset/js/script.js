bfdfa9fa976d4dcefaf816a40698799be922be6d
amirmmd1386 <153438005+amirmmd1386@users.noreply.github.com>
2024-04-29 08:35:10 -0700

    Update script.js

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,4 +1,4 @@
-import { quiz } from "/component/quiz/script.js";
-import { num } from "/component/numberAnimate/script.js";
+import { quiz } from "/QashqaiPage/component/quiz/script.js";
+import { num } from "/QashqaiPage/component/numberAnimate/script.js";
 window.customElements.define('quiz-box' , quiz)
 window.customElements.define('num-box' , num)
